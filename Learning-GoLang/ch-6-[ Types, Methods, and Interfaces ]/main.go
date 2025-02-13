3773fba6403cd8b16f8e97cb64c458ff210f75cf
Sahil Rana <sahilrana27582@gmail.com>
2025-02-13 20:48:24 +0530

    Methods Part - 2

--- Learning-GoLang/ch-6-[ Types, Methods, and Interfaces ]/main.go
+++ Learning-GoLang/ch-6-[ Types, Methods, and Interfaces ]/main.go
@@ -28,5 +28,45 @@ func main() {
 		age:       23,
 	}
 	sahil.personInfo()
+	sahil.updateName("King", "Thakur")
+	// Output : FirstName: King, LastName: Thakur
 
+	sahil.updateNameCopy(24)
+	fmt.Println(sahil.age)
+	// Output : 23
+
+}
+
+/*
+	Pointer Receiver vs Value Receiver
+
+	Pointer Receiver methods are those methods which are attached to the pointer of the struct.
+	They can change the original value of the struct or the receiver
+
+	func(p *Person) updateName(newName string){
+		p.name = name
+		fmt.Printf("Updated Name: %s", p.name)
+
+		This way we can change the original value of the receiver.
+	}
+*/
+
+func (p *Pesrons) updateName(newFirstName, newLastName string) {
+	p.firstName = newFirstName
+	p.lastName = newLastName
+	fmt.Println(p.firstName + " " + p.lastName)
+}
+
+/*
+	Value Receiver methods take the parameter as a copy of the original value.
+	So, if we even change the value of the receiver, it will still not change the original value.
+
+	func (p Person) updateNameCopy(age int){
+		p.age = age
+		fmt.Printf("Updated Age: %d", p.age)
+	}
+*/
+
+func (p Pesrons) updateNameCopy(age int) {
+	p.age = age
 }
