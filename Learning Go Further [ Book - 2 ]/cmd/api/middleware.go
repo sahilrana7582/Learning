62bea1f57b0cb87ae40915c86080bdddb2576e92
Sahil Rana <sahilrana27582@gmail.com>
2025-05-26 22:38:22 +0530

    Add Authorization Middleware

--- Learning Go Further [ Book - 2 ]/cmd/api/middleware.go
+++ Learning Go Further [ Book - 2 ]/cmd/api/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -17,6 +18,39 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 			}
 
 		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Add("Vary", "Authorization")
+
+		authorizationHeader := r.Header.Get("Authorization")
+
+		if authorizationHeader == "" {
+			app.badRequestResponse(w, r, fmt.Errorf("missing or invalid Authorization header"))
+			return
+		}
+
+		headerParts := strings.Split(authorizationHeader, " ")
+		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+			app.badRequestResponse(w, r, fmt.Errorf("invalid Authorization header format"))
+			return
+		}
+
+		token := headerParts[1]
+		if token == "" {
+			app.badRequestResponse(w, r, fmt.Errorf("missing token in Authorization header"))
+			return
+		}
+
+		if token != "ABC" {
+			app.invalidCredentialsResponse(w, r)
+			return
+		}
+		fmt.Println("Token is valid, proceeding with request")
 		next.ServeHTTP(w, r)
 	})
 }
